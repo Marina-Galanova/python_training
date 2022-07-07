60685431855bf839d013aaf05b7f59b4bd9a5e11
Marina Galanova <galanovam.m@mail.ru>
2022-07-07 10:38:55 +0700

    Рефакторинг файла test_add_group. Добавление файла test_update_group

--- test_add_group.py
+++ test_add_group.py
@@ -12,29 +12,48 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 class TestAddGroup():
-
   def setup_method(self, method):
     self.wd = webdriver.Firefox()
     self.vars = {}
 
-    def teardown_method(self, method):
-      self.wd.quit()
-
-  def test_add_group(self):
+  def open_home_page(self):
     self.wd.get("http://localhost/addressbook/")
     self.wd.set_window_size(1936, 1056)
+
+  def login(self):
     self.wd.find_element(By.NAME, "user").send_keys("admin")
     self.wd.find_element(By.NAME, "pass").click()
     self.wd.find_element(By.NAME, "pass").send_keys("secret")
     self.wd.find_element(By.CSS_SELECTOR, "input:nth-child(7)").click()
+
+  def open_groups_page(self):
     self.wd.find_element(By.LINK_TEXT, "groups").click()
+
+  def create_group(self):
+    # init group creation
     self.wd.find_element(By.NAME, "new").click()
+    # fill group form
     self.wd.find_element(By.NAME, "group_name").click()
     self.wd.find_element(By.NAME, "group_name").click()
     self.wd.find_element(By.NAME, "group_name").send_keys("group")
     self.wd.find_element(By.NAME, "group_header").send_keys("group header")
     self.wd.find_element(By.NAME, "group_footer").send_keys("group footer")
+    # submit group creation
     self.wd.find_element(By.NAME, "submit").click()
+
+  def return_to_groups_page(self):
     self.wd.find_element(By.LINK_TEXT, "groups").click()
+
+  def logout(self):
     self.wd.find_element(By.LINK_TEXT, "Logout").click()
-  
+
+  def test_add_group(self):
+    self.open_home_page()
+    self.login()
+    self.open_groups_page()
+    self.create_group()
+    self.return_to_groups_page()
+    self.logout()
+
+  def teardown_method(self, method):
+    self.wd.quit()
